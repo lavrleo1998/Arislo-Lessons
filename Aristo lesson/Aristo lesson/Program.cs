f942521baec88ea0a4ff6cd495c8a3fb5b915357
Леонов Лаврений <67091193+lavrleo1998@users.noreply.github.com>
2020-11-21 20:46:12 +0300

    1

---
+++ Aristo lesson/Aristo lesson/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aristo_lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pupil pupil1 = new Pupil("Иван", "Иванов","9А");
+            Pupil pupil2 = new Pupil("Макар", "Макаров", "9А");
+            Pupil pupil3 = new Pupil("Петр", "Петров", "9А");
+
+            Console.WriteLine("Выберите ученика");
+            Console.WriteLine("1)" + pupil1.Name + " " + pupil1.Surname);
+            Console.WriteLine("2)" + pupil2.Name + " " + pupil2.Surname);
+            Console.WriteLine("3)" + pupil3.Name + " " + pupil3.Surname);
+
+            int who = Convert.ToInt32(Console.ReadLine());
+            if (who == 1)
+            {
+                Console.WriteLine("Ты сделал домашнее задание?");
+                pupil1.Homework(Console.ReadLine());
+            }
+            if (who == 2)
+            {
+                Console.WriteLine("Ты сделал домашнее задание?");
+                pupil2.Homework(Console.ReadLine());
+            }
+            if (who == 3)
+            {
+                Console.WriteLine("Ты сделал домашнее задание?");
+                pupil3.Homework(Console.ReadLine());
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Aristo lesson/Aristo lesson/Pupil.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aristo_lesson
+{
+    class Pupil
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Сlassroom { get; set; }
+
+        public Pupil (string n, string s, string c)
+        {
+            this.Name = n;
+            this.Surname = s;
+            this.Сlassroom = c;
+        }
+
+        public void Homework(string answer)
+        {
+            if (answer == "yes")
+            {
+                Console.WriteLine("Молодец!");
+            }
+            else
+            {
+                Console.WriteLine("Молодец, два!");
+            }
+        }
+    }
+}
